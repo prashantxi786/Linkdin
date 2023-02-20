fb79918416b4a3e84d48448b6cdaa48a2255c75e
Prashant Singh <prashantxi786@gmail.com>
2023-02-20 18:06:42 +0530

    A

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.get("/",(req, res) => {
+    res.send("WELCOME TO LINKDIN")
+})
 app.use("/users",userRouter)
 app.use("/posts",postRouter)
 app.listen(process.env.port,async()=>{
